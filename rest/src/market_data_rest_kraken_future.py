f9ef5db89689559751bae3cee4c42f82db5fdb61
Arthurim <arthurbdauphine@gmail.com>
2023-03-22 21:53:30 +0000

    feat: add kraken future api - closes #15
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>

---
+++ rest/src/market_data_rest_kraken_future.py
@@ -0,0 +1,279 @@
+import base64
+import datetime
+import hashlib
+import hmac
+import urllib
+
+import pandas as pd
+
+from core.src.column_names import PRICE, MARKET_TIMESTAMP, GATEWAY_TIMESTAMP, SYM, MARKET, BID_PRICES, ASK_SIZES, \
+    ASK_PRICES, \
+    BID_SIZES, SIZE, MISC, FEES, FEES_MAKER, FEES_TAKER, RESULT, PAIR, LOW, OPEN, CLOSE, HIGH, TIME, BIDS, ASKS, SYMBOL
+from core.src.date import get_current_timestamp, today_date, MINUTES_PER_DAY, timestamp_to_date, add_days_to_date, \
+    to_date
+from core.src.instrument_types import SPOT, FUTURE
+from core.src.markets import KRAKEN
+from core.src.syms import split_currency_pair_into_lhs_rhs, BTC
+from rest.src.market_data_rest import MarketDataRestApi
+from rest.src.request_types import POST, GET
+
+
+class MarketDataRestApiKrakenFuture(MarketDataRestApi):
+    """
+    A base class for Kraken Spot Api
+    """
+
+    def __init__(self,
+                 public_path='/',
+                 private_path='/',
+                 public_key=None,
+                 private_key=None):
+        """
+
+        :param public_path:
+        :param private_path:
+        :param public_key:
+        :param private_key:
+        """
+        super().__init__(KRAKEN, FUTURE, public_path, private_path, public_key, private_key)
+
+    def _sign(self, data, url_path):
+        """
+        Sign request data according to Kraken's scheme.
+        :param data: dict, API request parameters
+        :param url_path: str, API URL path sans host
+        :returns: signature digest
+        """
+        nonce = self._nonce()
+        message = data + nonce + url_path
+
+        # step 2: hash the result of step 1 with SHA256
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(message.encode('utf8'))
+        hash_digest = sha256_hash.digest()
+
+        # step 3: base64 decode private_key
+        secretDecoded = base64.b64decode(self.private_key)
+
+        # step 4: use result of step 3 to has the result of step 2 with HMAC-SHA512
+        hmac_digest = hmac.new(secretDecoded, hash_digest, hashlib.sha512).digest()
+
+        # step 5: base64 encode the result of step 4 and return
+        return base64.b64encode(hmac_digest)
+
+    def format_sym_for_market(self, sym):
+        """
+        Format the sym to the exchange format
+
+        :param sym: str
+        :return: str
+        """
+        # Sometimes it is XBT sometimes BTC
+        # needs Z prefix before fiat and X before crypto part
+
+        crypto, fiat = split_currency_pair_into_lhs_rhs(sym)
+        if crypto == BTC:
+            crypto = 'XBT'
+        sym = "X" + crypto + "Z" + fiat
+        return sym
+
+    def format_sym_back(self, sym):
+        """
+        Format the sym back from the exchange format to our standard format
+
+        :param sym:
+        :return:
+        """
+        # fiat is always 3 letters
+        fiat = sym[-3:]
+        fiat = self.format_fiat_back(sym)
+        crypto = self.format_crypto_back(sym[:-4])
+        return crypto + fiat
+
+    def format_crypto_back(self, crypto):
+        """
+        Format the crypto back from the exchange format to our standard forma
+
+        :param crypto:
+        :return:
+        """
+        if len(crypto) == 3:
+            return crypto
+        elif crypto[0] == 'X':
+            return crypto[1:]
+        else:
+            raise Exception(f'Failed to convert back to fiat: {crypto}')
+
+    def format_fiat_back(self, fiat):
+        """
+        Format the fiat back from the exchange format to our standard forma
+
+        :param fiat:
+        :return:
+        """
+        if len(fiat) == 3:
+            return fiat
+        elif fiat[0] == 'Z':
+            return fiat[1:]
+        else:
+            raise Exception(f'Failed to convert back to fiat: {fiat}')
+
+    def process_error(self, response):
+        """
+        Used to process an error received from the endpoint
+
+        :param response: session.response
+        :return:
+        """
+        pass
+
+    def get_ticker_info(self, sym):
+        """
+        Returns ticker info as described:
+        https://docs.kraken.com/rest/#tag/Market-Data/operation/getTickerInformation
+
+        :param sym:
+        :return:
+        """
+        ticker = self.format_sym_for_market(sym)
+        ticker_info = self._query_public(method="Ticker", data={PAIR: ticker}, request_type=POST)
+        return ticker_info
+
+    def get_tob_bid(self, sym) -> float:
+        """
+
+        :return: float, top of book (tob) bid price
+        """
+        ticker = self.format_sym_for_market(sym)
+        ticker_info = self.get_ticker_info(sym)
+        bid = ticker_info[RESULT][ticker]['b'][0]
+        return float(bid)
+
+    def get_tob_ask(self, sym) -> float:
+        """
+
+        :param sym: str
+        :return: float, top of book (tob) ask price
+        """
+        ticker = self.format_sym_for_market(sym)
+        ticker_info = self.get_ticker_info(sym)
+        ask = ticker_info[RESULT][ticker]['a'][0]
+        return float(ask)
+
+    def get_tob_mid(self, sym) -> float:
+        """
+
+        :param sym: str
+        :return: float, top of book (tob) mid price
+        """
+        bid = self.get_tob_bid(sym)
+        ask = self.get_tob_ask(sym)
+        mid = 0.5 * (bid + ask)
+        return mid
+
+    def get_tob_spread(self, sym) -> float:
+        """
+
+        :param sym: str
+        :return: float, top of book (tob) spread
+        """
+        bid = self.get_tob_bid(sym)
+        ask = self.get_tob_ask(sym)
+        spread = ask - bid
+        return spread
+
+    def get_orderbook(self, sym, n_levels):
+        """
+        Returns the orderbook for the first n_levels
+
+        :param sym: str
+        :param n_levels: int
+        :return: an orderbook, i.e. a pd.DataFrane with columns [TIMESTAMP, GATEWAY_TIMESTAMP, SYM, MARKET,
+            BID_SIZES, BID_PRICES,ASK_SIZES, ASK_PRICES, MISC]
+        """
+
+        ticker = sym  # self.format_sym_for_market(sym)
+        result = self._query_public(method="orderbook", params={SYMBOL: ticker}, request_type=GET)
+        print(result)
+        data_ob = result['orderBook']
+        timestamp = result['serverTime']
+        format_string = "%Y-%m-%dT%H:%M:%S.%fZ"
+        timestamp = datetime.datetime.strptime(timestamp, format_string)
+        # Convert bids and asks to DataFrames
+        bids_df = pd.DataFrame(data_ob[BIDS], columns=[PRICE, SIZE])
+        asks_df = pd.DataFrame(data_ob[ASKS], columns=[PRICE, SIZE])
+
+        # Convert price and size columns to numeric type
+        bids_df[[PRICE, SIZE]] = bids_df[[PRICE, SIZE]].apply(pd.to_numeric)
+        asks_df[[PRICE, SIZE]] = asks_df[[PRICE, SIZE]].apply(pd.to_numeric)
+
+        ob = pd.DataFrame(data=[[bids_df[SIZE].tolist()[:n_levels],
+                                 bids_df[PRICE].tolist()[:n_levels],
+                                 asks_df[SIZE].tolist()[:n_levels],
+                                 asks_df[PRICE].tolist()[:n_levels]]],
+                          columns=[BID_SIZES, BID_PRICES, ASK_SIZES, ASK_PRICES])
+        ob[SYM] = sym
+        ob[MARKET] = self.market
+        ob[MARKET_TIMESTAMP] = timestamp
+        ob[GATEWAY_TIMESTAMP] = get_current_timestamp()
+        ob[GATEWAY_TIMESTAMP] = ob[GATEWAY_TIMESTAMP].apply(
+            lambda x: datetime.datetime.fromtimestamp(x))
+        ob[MISC] = ''
+        return ob[[MARKET_TIMESTAMP, GATEWAY_TIMESTAMP, SYM, MARKET, BID_SIZES, BID_PRICES, ASK_SIZES,
+                   ASK_PRICES, MISC]]
+
+    def get_ohlc(self, sym, since=None, interval=None):
+        ticker = self.format_sym_for_market(sym)
+        data = {PAIR: ticker}
+        if since is not None:
+            data['since'] = since
+        if interval is not None:
+            data['interval'] = interval
+        result = self._query_public(method="OHLC", data=data, request_type=POST)
+        result = result[RESULT][ticker]
+        cols = [TIME, OPEN, HIGH, LOW, CLOSE, "vwap", "volume", "count"]
+        ohlc = pd.DataFrame(result, columns=cols)
+        ohlc[TIME] = ohlc[TIME].apply(lambda x: datetime.datetime.fromtimestamp(x))
+        ohlc[[OPEN, CLOSE, HIGH, LOW]] = ohlc[[OPEN, CLOSE, HIGH, LOW]].apply(pd.to_numeric)
+        return ohlc
+
+    def get_close(self, sym, d=today_date()):
+        """
+        Returns the closing price at date d for sym
+
+        :param sym: str
+        :param d: timestamp
+        :return: float
+        """
+        if type(d) == int:
+            d = timestamp_to_date(d)
+        start_date = min(today_date(), d)
+        days_720_ago = add_days_to_date(today_date(), -720)
+        if start_date < days_720_ago:
+            raise Exception(f'kraken OHLC is broken and will not be able to get data for: {start_date}')
+        ohlc = self.get_ohlc(sym, interval=MINUTES_PER_DAY)
+        ohlc[TIME] = ohlc[TIME].apply(lambda x: to_date(x))
+
+        subset = ohlc.query(f'{TIME} == @start_date')
+        if len(subset) > 0:
+            close_price = subset[CLOSE].values[0]
+        else:
+            raise Exception(f'Failed to get Kraken close for {start_date}')
+        return close_price
+
+    def get_fee_schedule(self, sym):
+        """
+        Retrieves the fee schedule
+
+        :param sym: str
+        :return: dict, with keys ['fees_taker', 'fees_maker', 'fee_volume_currency']
+        """
+        fees = {}
+        ticker = self.format_sym_for_market(sym)
+        result = self._query_public(method="AssetPairs", data={PAIR: ticker}, request_type=POST)
+        result = result[RESULT][ticker]
+        fees[FEES_TAKER] = result[FEES]
+        fees[FEES_MAKER] = result[FEES_MAKER]
+        fee_currency = result['fee_volume_currency']
+        fees['fee_volume_currency'] = self.format_fiat_back(fee_currency)
+        return fees
